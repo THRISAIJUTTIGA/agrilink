2e328bfb50b6919189d057cf5cdde843661721d7
THRISAIJUTTIGA <thrisai06@gmail.com>
2026-09-06 10:15:02 +0530

    Update BASE_URL to live Render backend

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:4000/api';
+const BASE_URL = 'https://agrilink-d26r.onrender.com/api';
 
 async function request(path, options = {}) {
   const res = await fetch(`${BASE_URL}${path}`, {
